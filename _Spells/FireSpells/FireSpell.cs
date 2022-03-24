cc35d9f4f2e0146cebd888b0ef2547b353bd1b2c
LevyMatthew <matthewlevy24@gmail.com>
2022-03-23 21:10:42 -0400

    Fire

--- _Spells/FireSpells/FireSpell.cs
+++ _Spells/FireSpells/FireSpell.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(KinematicTracker))]
 public class FireSpell : Spell
 {
     public float velocityMultiplier = 10f;
     public GameObject punchSpawnTemplate;
+    public KinematicTracker kinematicTracker;
     protected Rigidbody rb;
 
     public override void Begin(SpellCaster caster, Transform target)
     {
-        base.Begin(caster, target);
+        base.Begin(caster, target);        
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.isKinematic = true;
@@ -19,14 +19,27 @@ public class FireSpell : Spell
 
     public override void Hold()
     {
-        rb.rotation = target.rotation;
-        rb.position = target.position;
+        Debug.Log(kinematicTracker.velocity);
+        if (kinematicTracker.velocity.magnitude > 1f)
+        {
+            Debug.Log("Spell Said punch");
+            TryDirectionalSpawn(kinematicTracker.velocity);
+        }
     }
 
     public override void Punch(Vector3 velocity)
     {
-        Debug.Log(velocity.magnitude);
         base.Punch(velocity);
+        TryDirectionalSpawn(velocity);
+    }    
+
+    public override void Release(Vector3 velocity)
+    {
+        Destroy(gameObject);
+    }
+
+    public void TryDirectionalSpawn(Vector3 velocity)
+    {
         if (punchSpawnTemplate != null)
         {
             GameObject punchInstance = GameObject.Instantiate(punchSpawnTemplate, transform.position, transform.rotation);
@@ -34,10 +47,6 @@ public class FireSpell : Spell
             punchRigidbody.velocity = velocity * velocityMultiplier;
             Destroy(punchInstance, 0.8f);
         }
-    }    
 
-    public override void Release(Vector3 velocity)
-    {
-        Destroy(gameObject);
     }
 }
